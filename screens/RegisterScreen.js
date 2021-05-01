6a660e07ae0c42c449de923ee6948bb53cd8ba06
Maddie02 <madlensarkisian@gmail.com>
2021-05-01 17:32:10 +0300

    Create register page with custom button

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import GuestLayout from '../components/GuestLayout';
-import { Text } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+import HydroButton from '../components/HydroButton';
 
 const RegisterScreen = () => {
+
+    const [email, setEmail] = useState('');
+    const [phone, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleRegistration = () => {
+        console.log('reg');
+    }
+
     return (
         <GuestLayout title="Регистрация">
-            
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+                <TextInput 
+                    placeholder="Имейл адрес"
+                    onChangeText={(email) => setEmail(email)}
+                    style={styles.input}
+                />
+                <TextInput 
+                    placeholder="Телефонен номер"
+                    onChangeText={(number) => setPhoneNumber(number)}
+                    style={styles.input}
+                />
+                <TextInput 
+                    placeholder="Парола"
+                    secureTextEntry={true}
+                    style={styles.input}
+                    onChangeText={(password) => setPhoneNumber(password)}
+                />
+                <HydroButton title="Регистрирай се" onPress={handleRegistration}/>
+            </KeyboardAvoidingView>
         </GuestLayout>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#7C7C7C',
+        marginBottom: 10,
+    },
+})
 
 export default RegisterScreen;
